600a4b3b556aba9a5fac9454f8d29e8020fd3871
md.nurul <nurulislam8433@gmail.com>
2025-12-05 11:08:56 +0600

    reduce

--- reduce.js
+++ reduce.js
@@ -5,3 +5,19 @@ const total = arr.reduce((acc, curr, index) => {
 }, "");
 
 console.log(total);
+
+const numbers = [1, 2, 3, 4, 5];
+
+const average = numbers.reduce((accumulator, currentvalue, index, array) => {
+  // if (currentvalue !== 4) {
+  //   accumulator.push(currentvalue);
+  // }
+  accumulator += currentvalue;
+
+  if (index === array.length - 1) {
+    return accumulator / array.length;
+  }
+  return accumulator;
+}, 0);
+
+console.log(average);
